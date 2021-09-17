ece12d01f5622eed568054031ec7bfe4adb74d3e
MartinDmont <martinus.cremers@outlook.fr>
2021-09-17 19:13:45 +0200

    first dbhandler

--- back/data_handler/dbHandler.py
+++ back/data_handler/dbHandler.py
@@ -1,1 +1,12 @@
 import psycopg2
+
+class Logger(object):
+    def __init__(self) -> None:
+        self.con = psycopg2.connect(
+            dbname='',
+            host='',
+            user='',
+            password='',
+            port=''
+        )
+        self.cur = self.con.cursor()
